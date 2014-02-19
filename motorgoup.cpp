69fff98b2e1c39b0a06f2805a672d017bb9f2619
electromatter <electromatter@gmail.com>
2014-02-18 20:39:21 -0500

    Update motorgoup.cpp

--- motorgoup.cpp
+++ motorgoup.cpp
@@ -1,12 +1,12 @@
 #include "motorgroup.h"
 
-motorgroup::motorgroup(int pwma, int pwmb, int dir) : m_sp(1), m_p(1), m_i(0), m_d(0), m_last(0), m_int(0), m_tlast(0), m_it(0), m_a(NULL), m_b(NULL), m_enc(NULL), m_dir(dir)
+motorgroup::motorgroup(int pwma, int pwmb, int dir) : m_a(NULL), m_b(NULL), m_enc(NULL), m_dir(dir)
 {
 	m_a = new Talon(pwma);
 	m_b = new Talon(pwmb);
 }
 
-motorgroup::motorgroup(int pwma, int pwmb, int enca, int encb) : m_sp(1), m_p(1), m_i(0), m_d(0), m_last(0), m_int(0), m_tlast(0), m_it(0), m_a(NULL), m_b(NULL), m_enc(NULL), m_dir(0)
+motorgroup::motorgroup(int pwma, int pwmb, int enca, int encb) : m_a(NULL), m_b(NULL), m_enc(NULL), m_dir(0)
 {
 	m_a = new Talon(pwma);
 	m_b = new Talon(pwmb);
@@ -26,30 +26,7 @@ motorgroup::~motorgroup()
 
 void motorgroup::speed(float sp)
 {
-	float e = get() * m_sp - sp;
-	float pwm;
-	float dt = deltat(m_tlast);
-	float de = e - m_last;
-	m_last = e;
-	if (m_it == 0) {
-		pwm = m_p * e;
-	} else {
-		m_int += e * dt;
-		pwm = m_p * e + m_i * m_int + m_d * de;
-	}
-	m_it++;
-	set(pwm);
-}
-
-void motorgroup::params(float sp, float p, float i, float d)
-{
-	m_sp = sp;
-	m_p = p;
-	m_i = i;
-	m_d = d;
-	m_it = 0;
-	m_last = 0;
-	m_int = 0;
+	set(input(get(), sp));
 }
 
 void motorgroup::set(float pwm)
@@ -70,6 +47,7 @@ float motorgroup::get()
 
 void motorgroup::reset()
 {
+	pid::reset();
 	if (m_enc)
 		m_enc->Reset();
 }
