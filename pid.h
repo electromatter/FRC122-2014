b4f2585a15fce1e6d67d4e0d474d01493958c6ff
electromatter <electromatter@gmail.com>
2014-02-18 20:41:13 -0500

    Update pid.h

--- pid.h
+++ pid.h
@@ -4,4 +4,20 @@
 extern double time();
 extern double deltat(double& t);
 
+class pid
+{
+public:
+	pid(float sp = 0, float p = 1, float i = 0, float d = 0);
+	virtual ~pid();
+	float input(float x, float setpoint);
+	void params(float sp = 0, float p = 1, float i = 0, float d = 0);
+	void reset();
+private:
+	float m_sp, m_p, m_i, m_d;
+	float m_last;
+	float m_int;
+	double m_tlast;
+	int m_it;
+};
+
 #endif
